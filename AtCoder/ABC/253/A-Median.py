e83dc6d639b07addbfab51038e178b5562f633e8
JiwonChoi1017 <99472003+JiwonChoi1017@users.noreply.github.com>
2022-06-11 20:54:30 +0900

    Revert "20220611 AtCoder ABC253のA, Bを解決"

--- AtCoder/ABC/253/A-Median.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-# A - Median?
-# https://atcoder.jp/contests/abc253/tasks/abc253_a
-li = list(map(int, sys.stdin.readline().split()))
-li_sorted = sorted(li)
-result = "Yes" if li[1] == li_sorted[1] else "No"
-print(result)
